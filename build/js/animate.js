798ac05288f184fe47a2aedf50116a0954f74d1c
Grraoo <evil@grraoo.ru>
2017-10-22 14:37:13 +0800

    animated wall

--- build/js/animate.js
+++ build/js/animate.js
@@ -1,11 +1,8 @@
+const TIME = 1500;
 var templateItem = document.querySelector('template').content.querySelector('.wall__item');
+
 var wallSections = [].slice.call(document.querySelectorAll('.wall__section'));
-
-var animationClasses = ['wall__section--new-forward', 'wall__section--new-back'];
-
-function newStep (section, i) {
-  changeClass(section, animationClasses[i], true);
-}
+var container = document.querySelector('.wall');
 
 var createItem = function(wallSection, post, direction) {
   var newItem = templateItem.cloneNode(true);
@@ -17,15 +14,15 @@ var createItem = function(wallSection, post, direction) {
     newItem.style = 'background-image: url('+post.media+')';
   }
   wallSection.insertBefore(newItem, wallSection.firstChild);
-  newStep(wallSection, direction);
-
 }
+var counter = 0;
 
 data.forEach(function(post, i) {
+  // counter++;
   console.log(post.author);
+  console.log((i+1) % data.length);
 });
 
-var counter = 0;
 var delOldItem = function(section, qnt){
 
   var oldItem = section.children[qnt];
@@ -34,20 +31,30 @@ var delOldItem = function(section, qnt){
   }
 };
 
-var swipeWall = function() {
-
-  delOldItem(wallSections[0], 5);
+var swipeWall = function(container) {
+  var section = container.children[counter % 2];
+  delOldItem(section, 5);
   setTimeout(function() {
-    createItem(wallSections[0], data[counter], 0);
+    createItem(section, data[counter % data.length], 0);
     counter++;
-    console.log(wallSections[0].children.length);
-    if(counter < data.length) {
-      swipeWall();
-    } else {
-      console.log('ok');
-      delOldItem(wallSections[0], 5);
+    if(counter % 8 != 0) {
+      swipeWall(container);
     }
-  }, 1500);
+    else {
+      console.log('ok');
+      toggleBanner(container);
+    }
+  }, TIME);
+};
 
-  };
-  swipeWall();
+function toggleBanner(container) {
+  setTimeout(function() {
+    changeClass(container, 'show-banner', T);
+    setTimeout(function() {
+      changeClass(container, 'show-banner', T);
+      swipeWall(container);
+    }, TIME);
+  }, TIME);
+}
+
+toggleBanner(container);
--- build/js/data.js
+++ build/js/data.js
@@ -1,51 +1,99 @@
 var data = [
   {
-    author: 'MosInjProject',
+    author: '1MosInjProject',
     social: 'tw',
     media: null,
     text: 'По главной улице пройду — и солнце светит. Обнял всех вас!'
   },
   {
-    author: 'Tanya Krav',
+    author: '2Tanya Krav',
     social: 'tw',
     media: null,
     text: 'Что отражаем, то и порождаем. Я металлический'
   },
   {
-    author: 'Николай Булкин',
+    author: '3Николай Булкин',
     social: 'inst',
     media: 'img/wall1.png',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, labore?'
   },
   {
-    author: 'Kirill Borisov',
+    author: '4Kirill Borisov',
     social: 'fb',
     media: 'img/wall2.jpg',
     text: 'Этим кибернетическим сообщением в сети «Инстаграмм» я передаю привет в дождливую и холодную Москву из солнечной летней Сибири. Держитесь, друзья...'
   },
   {
-    author: 'Степан Луков',
+    author: '5Степан Луков',
     social: 'tw',
     media: null,
     text: 'Правильный чай: шен-пуэр с лимоном из гайвани. Тепла и уюта в вашу жизнь!'
   },
   {
-    author: 'Sasha Reushkin',
+    author: '6Sasha Reushkin',
     social: 'inst',
     media: 'img/wall3.jpg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, deleniti?'
   },
   {
-    author: 'Andrey Okonetchnikov',
+    author: '7Andrey Okonetchnikov',
     social: 'tw',
     media: 'img/wall4.jpg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, deleniti?'
   },
   {
-    author: 'Tanya Krav',
+    author: '8Tanya Krav',
     social: 'vk',
     media: 'img/wall5.jpg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, deleniti?'
   },
+  {
+    author: '9MosInjProject',
+    social: 'tw',
+    media: null,
+    text: 'По главной улице пройду — и солнце светит. Обнял всех вас!'
+  },
+  {
+    author: '10Tanya Krav',
+    social: 'tw',
+    media: null,
+    text: 'Что отражаем, то и порождаем. Я металлический'
+  },
+  {
+    author: '11Николай Булкин',
+    social: 'inst',
+    media: 'img/wall1.png',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, labore?'
+  },
+  {
+    author: '12Kirill Borisov',
+    social: 'fb',
+    media: 'img/wall2.jpg',
+    text: 'Этим кибернетическим сообщением в сети «Инстаграмм» я передаю привет в дождливую и холодную Москву из солнечной летней Сибири. Держитесь, друзья...'
+  },
+  {
+    author: '13Степан Луков',
+    social: 'tw',
+    media: null,
+    text: 'Правильный чай: шен-пуэр с лимоном из гайвани. Тепла и уюта в вашу жизнь!'
+  },
+  {
+    author: '14Sasha Reushkin',
+    social: 'inst',
+    media: 'img/wall3.jpg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, deleniti?'
+  },
+  {
+    author: '15Andrey Okonetchnikov',
+    social: 'tw',
+    media: 'img/wall4.jpg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, deleniti?'
+  },
+  {
+    author: '16Tanya Krav',
+    social: 'vk',
+    media: 'img/wall5.jpg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, deleniti?'
+  }
 
 ];
